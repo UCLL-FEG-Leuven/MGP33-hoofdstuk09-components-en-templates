73efe87dbce93638bb043ead84b61d68094072c8
David Urting <David.Urting@lv.vlaanderen.be>
2024-03-23 12:16:25 +0100

    Feature 6 toegevoegd: component navigatie

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,24 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { KlokComponent } from './klok/klok.component';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterOutlet, KlokComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  newTimeZone: string = 'Europe/Brussels';
-  timeZones: Array<string> = [];
-
-  addNewTimeZone() {
-    this.timeZones.push(this.newTimeZone);
-    this.newTimeZone = "";
-  }
 }
-
-  // 'America/New_York', 'Europe/Brussels', 'Japan'
---
+++ Client/src/app/klokken/klokken.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { KlokComponent } from '../klok/klok.component';
+
+@Component({
+  selector: 'app-klokken',
+  standalone: true,
+  imports: [FormsModule, KlokComponent],
+  templateUrl: './klokken.component.html',
+  styleUrl: './klokken.component.css'
+})
+export class KlokkenComponent {
+  newTimeZone: string = 'Europe/Brussels';
+  timeZones: Array<string> = [];
+
+  addNewTimeZone() {
+    this.timeZones.push(this.newTimeZone);
+    this.newTimeZone = "";
+  }
+}
