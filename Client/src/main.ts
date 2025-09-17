3c9ec82fa6fe106570249a8151952072c64a09b1
David Urting <David.Urting@lv.vlaanderen.be>
2025-09-18 00:04:12 +0200

    app.config.ts correct gewired

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,11 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-import { provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 
-bootstrapApplication(App, {providers: [
-  provideZonelessChangeDetection(),
-  provideRouter(routes)
-]})
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(App, appConfig)
 .catch((err) => console.error(err));;
